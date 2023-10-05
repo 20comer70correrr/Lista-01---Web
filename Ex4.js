b09ab653ca6d3dd5f40d0b84ceff501df21f0bc3
20comer70correrr <108698700+20comer70correrr@users.noreply.github.com>
2023-10-05 18:56:38 -0300

    Update and rename main.js to Ex4.js

---
+++ Ex4.js
@@ -0,0 +1,10 @@
+const numero1 = 81
+const numero2 = 9
+//
+//
+const soma = numero1 + numero2
+const subtração = numero1 - numero2
+const multiplicação = numero1 * numero2
+const divisao = numero1 / numero2
+//
+Console.log (soma, subtração, multiplicação, divisao)
